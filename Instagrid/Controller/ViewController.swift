1ac266db8a5c0bab0468b3d62e24ccbb399f44e9
Ousama boujaouane <oboujaouane@gmail.com>
2020-08-13 18:45:40 +0200

    Remove LayoutButtonsView and adapt main storyboard and ViewController

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,28 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // MARK: - Internal Outlet
+    // MARK: - Internal Outlets
     
     @IBOutlet weak var gridView: GridView?
+    @IBOutlet private var oneTwoSelectionImage: UIImageView?
+    @IBOutlet private var twoOneSelectionImage: UIImageView?
+    @IBOutlet private var twoTwoSelectionImage: UIImageView?
     
-    // MARK: - Private Outlet
     
-    @IBOutlet private weak var layoutButtonsView: LayoutButtonsView?
+    // MARK: - Private property
     
-    // MARK: - Life cycle
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    private var selectedButton = 0 {
+        didSet {
+            showSelectionForButton(selectedButton)
+        }
     }
     
-    // MARK: - Internal function
-    
-    func changeGridLayout(numberOfColumnsForFirstLine: Int, numberOfColumnsForSecondLine: Int) {
+    // MARK: - Private functions
+
+    private func changeGridLayout(numberOfColumnsForFirstLine: Int, numberOfColumnsForSecondLine: Int) {
         gridView?.numberOfColumnsForFirstLine = numberOfColumnsForFirstLine
         gridView?.numberOfColumnsForSecondLine = numberOfColumnsForSecondLine
     }
+    
+    private func showSelectionForButton(_ index: Int) {
+        switch index {
+        case 0:
+            oneTwoSelectionImage?.isHidden = false
+            twoOneSelectionImage?.isHidden = true
+            twoTwoSelectionImage?.isHidden = true
+        case 1:
+            oneTwoSelectionImage?.isHidden = true
+            twoOneSelectionImage?.isHidden = false
+            twoTwoSelectionImage?.isHidden = true
+        case 2:
+            oneTwoSelectionImage?.isHidden = true
+            twoOneSelectionImage?.isHidden = true
+            twoTwoSelectionImage?.isHidden = false
+        default:
+            break
+        }
+    }
 
+    // MARK: - Action
+    
+    @IBAction private func buttonTouched(_ sender: UIButton) {
+            switch sender.tag {
+            case 0:
+                changeGridLayout(numberOfColumnsForFirstLine: 1, numberOfColumnsForSecondLine: 2)
+                selectedButton = sender.tag
+            case 1:
+                changeGridLayout(numberOfColumnsForFirstLine: 2, numberOfColumnsForSecondLine: 1)
+                selectedButton = sender.tag
+            case 2:
+                changeGridLayout(numberOfColumnsForFirstLine: 2, numberOfColumnsForSecondLine: 2)
+                selectedButton = sender.tag
+            default:
+                break
+            }
+    }
 
 }
 
