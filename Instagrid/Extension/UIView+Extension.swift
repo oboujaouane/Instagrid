f2ca4731046d504ae15cb5c4b24e2d735f427562
Ousama boujaouane <oboujaouane@gmail.com>
2020-08-25 15:53:15 +0200

    Clean

--- Instagrid/Extension/UIView+Extension.swift
+++ Instagrid/Extension/UIView+Extension.swift
@@ -11,26 +11,6 @@ import Foundation
 
 extension UIView {
     
-    public class func findByAccessibilityIdentifier(identifier: String) -> UIView? {
-        guard let window = UIApplication.shared.keyWindow else {
-            return nil
-        }
-        
-        func findByID(_ view: UIView, _ id: String) -> UIView? {
-            if view.accessibilityIdentifier == id {
-                return view
-            }
-            for v in view.subviews {
-                if let a = findByID(v, id) {
-                    return a
-                }
-            }
-            return nil
-        }
-        
-        return findByID(window, identifier)
-    }
-    
     // On étend la classe UIView avec une nouvelle propriété "image" comme screenshot. La méthode snapshotView() fournit par Apple renvoie un UIView non un UIImageView, utiliser snapshotView() nécessiterait donc en plus de caster la valeur de retour en UIImageView, la solution d'étendre la classe UIView est donc la plus simple et la plus rapide trouvée.
     var screenshot: UIImage? {
         let renderer = UIGraphicsImageRenderer(bounds: bounds)
